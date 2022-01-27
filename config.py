37372580d3f5eaea29e061404d6570831f55a376
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-27 10:15:43 +0300

    Upload Google Sheets related files

---
+++ config.py
@@ -0,0 +1,24 @@
+# Spreadsheet
+SPREADSHEET_TITLE = "Dvmn Team Project Schedule"
+
+# Worksheets
+STUDENTS_TITLE = "Students"
+STUDENTS_HEADERS = ["name", "level", "tg_username", "discord_username", "is_far_east"]
+
+PM_TITLE = "PM"
+PM_HEADERS = ["name", "time_slots", "tg_username", "discord_username"]
+
+# Worksheet headers style format
+HEADERS_STYLE = {
+    "backgroundColor": {
+        "red": 0.511,
+        "green": 0.511,
+        "blue": 0.511,
+    },  # RGB Normalized decimal color value
+    "horizontalAlignment": "CENTER",
+    "textFormat": {
+        "foregroundColor": {"red": 1.0, "green": 1.0, "blue": 1.0},
+        "fontSize": 12,
+        "bold": True,
+    },
+}
